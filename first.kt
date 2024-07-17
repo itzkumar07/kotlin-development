2f071c6b7fb39c1818bea38a49593854482137af
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2024-07-17 17:10:25 +0530

    fun added

--- first.kt
+++ first.kt
@@ -1,11 +1,15 @@
+fun main () {
+    frist_Code()
+}
 fun frist_Code() {
     var myVariable: Int = 50
     val myNumber: Int = 77
-
+    
     println("This is the Val value:- {$myNumber}")
     println(myVariable)
-    println()
     
+    println()  
+   
     myVariable = 12
     println("This is the Var Changed Value:- ${myVariable}")
 }
