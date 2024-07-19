4e7d3734f73788a4c56be696f0ad4edb444bcdd6
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2024-07-19 13:50:51 +0530

    Create concatenation_code_two.kt

---
+++ concatenation_code_two.kt
@@ -0,0 +1,11 @@
+fun main (){
+ Third_Code()
+}
+
+fun Third_Code() {
+    val Student_name = "Kumar Kankam "
+    val Roll_no = 11
+    val Div = " Sec-B"
+    val Sub = " Koltlin Progamming Language"
+    println("${Student_name+ Roll_no+ Div+ Sub}")
+}
