811dc9f65a016ba80799197b99bf98a5a2711b1c
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2024-07-20 08:13:41 +0530

    Create arithmetic_Oprators.kt

---
+++ arithmetic_Oprators.kt
@@ -0,0 +1,22 @@
+
+fun main (){
+ Arithmetic_Oprators()
+}
+
+fun Arithmetic_Oprators() {
+    val f_no = 100
+    val s_no = 22
+    
+    val tow_no_Add = f_no + s_no 
+    val tow_no_Sub = f_no - s_no
+    val tow_no_Div = f_no / s_no
+    val tow_no_Mul = f_no * s_no
+    val tow_no_Per = f_no % s_no
+   
+    println("This is the Arithmetic_Oprators")
+    println(tow_no_Add)
+    println(tow_no_Sub)
+    println(tow_no_Div)
+    println(tow_no_Mul)
+    println(tow_no_Per)
+}
