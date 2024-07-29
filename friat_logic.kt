0fa7ec9b6292eb30993ee9a117db93896d12af92
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2024-07-29 07:48:27 +0530

    First_logic file upload

---
+++ friat_logic.kt
@@ -0,0 +1,13 @@
+fun main() {
+    myname()
+   }
+fun myname(){
+    val voter_allow = 20
+    val voter_not_allow=1
+    if(voter_allow < voter_not_allow){
+      println("yes") 
+      }
+      else{
+          println ("no")
+      }
+}
