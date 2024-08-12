13e91ec46f5b36fbf984cf94348a53ff42fd15f9
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2024-08-12 22:36:39 +0530

    Create Datatypes.kt

---
+++ Datatypes.kt
@@ -0,0 +1,31 @@
+fun main() {
+    // Integer types
+    val byteNumber: Byte = 127
+    val shortNumber: Short = 32767
+    val intNumber: Int = 2147483647
+    val longNumber: Long = 9223372036854775807
+
+    // Floating-point types
+    val floatNumber: Float = 3.14F
+    val doubleNumber: Double = 3.141592653589793
+
+    // Character type
+    val character: Char = 'K'
+
+    // Boolean type
+    val isKotlinFun: Boolean = true
+
+    // String type
+    val string: String = "Hello, Kotlin!"
+
+    // Printing the values
+    println("Byte: $byteNumber")
+    println("Short: $shortNumber")
+    println("Int: $intNumber")
+    println("Long: $longNumber")
+    println("Float: $floatNumber")
+    println("Double: $doubleNumber")
+    println("Char: $character")
+    println("Boolean: $isKotlinFun")
+    println("String: $string")
+}
